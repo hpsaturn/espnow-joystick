ce0db026f8578e091fb89f64cfd06dc44e26d25e
Hpsaturn <hpsaturn@gmail.com>
2022-02-12 14:26:56 +0100

    first version using ESPNow and nanopb in the robot side

--- src/robot/robot.cpp
+++ src/robot/robot.cpp
@@ -1,6 +1,5 @@
 #include <WiFi.h>
-#include <WiFiUdp.h>
-#include <OTAHandler.h>
+#include <esp_now.h>
 #include <analogWrite.h>
 #include <pb_decode.h>
 #include <pb_encode.h>
@@ -12,11 +11,6 @@
 uint32_t count = 0;
 uint8_t IIC_ReState = I2C_ERROR_NO_BEGIN;
 
-const char *ssid = "ROBOTAP";
-const char *password = "77777777";
-
-WiFiServer server(80);
-WiFiUDP udp;
 uint8_t buffer[128];
 JoystickMessage jm = JoystickMessage_init_zero;
 
@@ -29,23 +23,6 @@ JoystickMessage jm = JoystickMessage_init_zero;
 #define MIN4  21
 
 ESP32MotorControl mc = ESP32MotorControl();
-
-bool decodeMessage(uint16_t message_length) {
-    /* This is the buffer where we will store our message. */
-
-    /* Create a stream that reads from the buffer. */
-    pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
-
-    /* Now we are ready to decode the message. */
-    bool status = pb_decode(&stream, JoystickMessage_fields, &jm);
-
-    /* Check for errors... */
-    if (!status) {
-        printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
-        return false;
-    }
-    return true;
-}
 
 void setSpeed(int16_t Vtx, int16_t Vty, int16_t Wt) {
     Wt = (Wt > 100) ? 100 : Wt;
@@ -102,36 +79,89 @@ void setSpeed(int16_t Vtx, int16_t Vty, int16_t Wt) {
 
 }
 
-void otaLoop() {
-    if (WiFi.isConnected()) {
-        ota.loop();
+bool decodeMessage(uint16_t message_length) {
+    /* This is the buffer where we will store our message. */
+
+    /* Create a stream that reads from the buffer. */
+    pb_istream_t stream = pb_istream_from_buffer(buffer, message_length);
+
+    /* Now we are ready to decode the message. */
+    bool status = pb_decode(&stream, JoystickMessage_fields, &jm);
+
+    /* Check for errors... */
+    if (!status) {
+        printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
+        return false;
     }
+    return true;
 }
 
-void otaInit() {
-    ota.setup("AIROBOT", "AIROBOT");
-    // ota.setCallbacks(new MyOTAHandlerCallbacks());
+void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength) {
+    snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
+}
+
+void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen) {
+    // only allow a maximum of 250 characters in the message + a null terminating byte
+    char udodata[ESP_NOW_MAX_DATA_LEN + 1];
+    int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
+    strncpy(udodata, (const char *)data, msgLen);
+    // make sure we are null terminated
+    udodata[msgLen] = 0;
+    // format the mac address
+    char macStr[18];
+    formatMacAddress(macAddr, macStr, 18);
+    // debug log the message to the serial port
+    Serial.printf("Received message from: %s - %s\n", macStr, buffer);
+    // what are our instructions
+    
+    // char udodata[dataLen];
+    // udp.read(udodata, udplength);
+    // IPAddress udp_client = udp.remoteIP();
+    if ((udodata[0] == 0xAA) && (udodata[1] == 0x55) && (udodata[msgLen - 1] == 0xee)) {
+        for (int i = 0; i < msgLen - 4; i++) {
+            buffer[i] = udodata[i + 3];
+        }
+        decodeMessage(msgLen - 4);
+        if (jm.ck == 0x01) {
+            setSpeed(jm.ax - 100, jm.ay - 100, jm.az - 100);
+        } else {
+            setSpeed(0, 0, 0);
+        }
+    } else {
+        setSpeed(0, 0, 0);
+    }
+    
+}
+
+// callback when data is sent
+void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status) {
+    char macStr[18];
+    formatMacAddress(macAddr, macStr, 18);
+    // Serial.print("Last Packet Sent to: ");
+    // Serial.println(macStr);
+    // Serial.print("Last Packet Send Status: ");
+    // Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 void setup() {
     Serial.begin(115200);
-    uint64_t chipid = ESP.getEfuseMac();
-    String str = ssid + String((uint32_t)(chipid >> 32), HEX);
-    //Set device in STA mode to begin with
-    WiFi.softAPConfig(IPAddress(192, 168, 4, 1),
-                      IPAddress(192, 168, 4, 1),
-                      IPAddress(255, 255, 255, 0));
+    delay(100);
+    WiFi.mode(WIFI_STA);
+    // startup ESP Now
+    Serial.println("ESPNow Init");
+    Serial.println(WiFi.macAddress());
+    // shut down wifi
+    WiFi.disconnect();
 
-    WiFi.softAP(str.c_str(), password);
-    IPAddress myIP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(myIP);
-    server.begin();
-
-
-    otaInit();
-
-    udp.begin(1003);
+    if (esp_now_init() == ESP_OK) {
+        Serial.println("ESPNow Init Success");
+        esp_now_register_recv_cb(receiveCallback);
+        esp_now_register_send_cb(sentCallback);
+    } else {
+        Serial.println("ESPNow Init Failed");
+        delay(3000);
+        ESP.restart();
+    }
 
     analogWriteResolution(BUILTINLED, 12);   // builtin LED for TTGO-T7 v1.3 (see docs directory)
 
@@ -139,27 +169,4 @@ void setup() {
 }
 
 void loop() {
-    int udplength = udp.parsePacket();
-    if (udplength) {
-        char udodata[udplength];
-        udp.read(udodata, udplength);
-        IPAddress udp_client = udp.remoteIP();
-        if ((udodata[0] == 0xAA) && (udodata[1] == 0x55) && (udodata[udplength - 1] == 0xee)) {
-            for (int i = 0; i < udplength-4; i++) {
-                buffer[i] = udodata[i+3];
-            }
-            decodeMessage(udplength-4);
-            if (jm.ck == 0x01) {
-                setSpeed(jm.ax - 100, jm.ay - 100, jm.az - 100);
-            } else {
-                setSpeed(0, 0, 0);
-            }
-        } else {
-            setSpeed(0, 0, 0);
-        }
-    }
-    else {
-        delay(10);
-    }
-    ota.loop();
 }
